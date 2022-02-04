b80e5fe456828a6f95967d21f5bfedb17e31f768
Jose <josesantillan10@gmail.com>
2022-02-04 17:06:41 -0600

    portfolio is stable, added js, css, html and test images, need to change images and maybe responsive design

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const toggleLight = () =>{
+	const skills = document.getElementById("skills");
+	if(skills){
+		skills.classList.toggle("light-active")
+	}
+}
+skills.addEventListener("click", toggleLight);
+toggleLight(); 
+
+const arraySkills = [];
+
+
+const addMovement = () => {
+	const list1 = document.getElementById("skillsList1");
+	const list2 = document.getElementById("skillsList2");
+	const getChildrens = [...list1.children,...list2.children]
+	const randomElement = getChildrens[Math.floor(Math.random() * getChildrens.length)];
+	if(!randomElement.classList.contains('activeSwinging')){
+		randomElement.classList.add('activeSwinging')
+		setTimeout(function(){
+			randomElement.classList.remove('activeSwinging')
+		}, 1500)
+	}
+}
+
+setInterval(addMovement, 1500);
